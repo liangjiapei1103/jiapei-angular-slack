f122e8ff14a74449966db186588253cea1db9eac
Jiapei Liang <liang98@purdue.edu>
2016-08-30 14:42:01 -0400

    Add presence to users

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -10,9 +10,14 @@ angular.module('angularfireSlackApp')
         channelsCtrl.getDisplayName = Users.getDisplayName;
         channelsCtrl.getGravatar = Users.getGravatar;
 
+        Users.setOnline(profile.$id);
+
         channelsCtrl.logout = function() {
-            Auth.$unauth();
-            $state.go('home');
+            channelsCtrl.profile.online = null;
+            channelsCtrl.profile.$save().then(function() {
+                Auth.$unauth();
+                $state.go('home');
+            });
         };
 
         channelsCtrl.newChannel = {
